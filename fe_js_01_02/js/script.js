4b1e519d4d118cd7ec978f13adc39b6275d247a6
olko06141 <sanystation@gmail.com>
2017-06-24 13:27:42 +0300

    some changes

--- fe_js_01_02/js/script.js
+++ fe_js_01_02/js/script.js
@@ -3,24 +3,27 @@ var names = [];
 var specifiedNamePos = -1;
 
 for (var i = 0; i < ARR_SIZE; ++i) {
-  names[i] = prompt('Please, enter the name ' + (i + 1));
+  names[i] = window.prompt('Please, enter the name ' + (i + 1));
 }
 
 do {
-  var specifiedName = prompt('Please, specify a name to find it or type "exit" to exit');
-  if ('exit' == specifiedName) break;
+  var specifiedName = window.prompt('Please, specify a name to find it or type "exit" to exit');
+
+  if ('exit' === specifiedName) {
+    break;
+  }
   
   for (var i = 0; i < ARR_SIZE; ++i) {
-    if (specifiedName == names[i]) {
+    if (specifiedName === names[i]) {
       specifiedNamePos = i;
       break;
     }
   }
-  
+
   if (specifiedNamePos < 0) {
-    alert('There is no such name: ' + specifiedName + '. Please, try again or type "exit" to exit');
+    window.alert('There is no such name: ' + specifiedName + '. Please, try again or type "exit" to exit');
   } else {
-    alert(specifiedName + ', you have logged in!');
+    window.alert(specifiedName + ', you have logged in!');
   }
 } while (specifiedNamePos < 0);
 
