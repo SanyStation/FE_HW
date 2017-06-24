52a44cf1624692f64710bac6710c1870844781f2
olko06141 <sanystation@gmail.com>
2017-06-24 13:20:15 +0300

    add new features

--- fe_js_01_01/js/script.js
+++ fe_js_01_01/js/script.js
@@ -6,32 +6,37 @@
  * @returns {number} x to the power of n
  */
 function pow(x, n) {
+  "use strict";
+
   x = Number(x);
   n = Number(n);
+
   if (!Number.isInteger(n)) {
     return NaN;
   }
+
   if (n === 0) {
     return x === 0 ? NaN : 1;
-  } else {
-    var tmpNumber = x;
-    var tmpPower = n;
-    while (--tmpPower > 0) {
-      x *= tmpNumber;
-    }
-    if (n > 0) {
-      return x;
-    } else {
-      return 1 / x;
-    }
   }
+
+  var tmpNumber = x;
+  var tmpPower = n;
+
+  while (--tmpPower > 0) {
+    x *= tmpNumber;
+  }
+
+  if (n > 0) {
+    return x;
+  }
+  return 1 / x;
 }
 
-var x = prompt('Please, enter the x');
-var n = prompt('Please, enter the n of x');
+var x = window.prompt('Please, enter the x');
+var n = window.prompt('Please, enter the n of x');
 var result = pow(x, n);
 if (Number.isNaN(result)) {
-  console.log('Function is not able to calculate. Please, check your input params: x = ' + x + '; n = ' + n + '. Power should be an integer value. Also be sure x and n do not equal 0 simultaneously.');
+  window.console.log('Function is not able to calculate. Please, check your input params: x = ' + x + '; n = ' + n + '. Power should be an integer value. Also be sure x and n do not equal 0 simultaneously.');
 } else {
-  console.log(x + '^' + n + ' = ', result);
+  window.console.log(x + '^' + n + ' = ', result);
 }
