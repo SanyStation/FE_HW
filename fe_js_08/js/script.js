17cede99ecc363ff5ce8f5635138c865f71c7c59
olko06141 <sanystation@gmail.com>
2017-07-29 14:27:53 +0300

    FE_JS-08: done!

--- fe_js_08/js/script.js
+++ fe_js_08/js/script.js
@@ -1,4 +1,8 @@
 $(function() {
   "use strict";
-
+  $('.form__pair__field').hover(function() {
+    $(this).next('.form__pair__hint').stop(true, true).animate({opacity: "show", left: 285}, "slow");
+  }, function() {
+    $(this).next('.form__pair__hint').stop(true, true).animate({opacity: "hide", left: 300}, "fast");
+  });
 });
