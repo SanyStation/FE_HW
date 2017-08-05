f94c47e4776a5044467d02c205647280ee323712
olko06141 <sanystation@gmail.com>
2017-08-05 18:30:05 +0300

    FE_JS_10: Done!

---
+++ fe_js_10/js/script.js
@@ -0,0 +1,79 @@
+(function($) {
+  "use strict";
+
+  /**
+   * Function is designed to draw menu by means of requestAnimationFrame.
+   *
+   * @param {XML/Node} menuObj - menu object which will be drown
+   * @param {Function} drawFunction - function which draws the menu
+   * @param {Number} duration - duration of animation in milliseconds
+   */
+  function drawMenu(menuObj, drawFunction, duration) {
+    if (duration < 1) {
+      duration = 1;
+    }
+    var start = performance.now();
+    requestAnimationFrame(function animate(time) {
+      var timePassed = time - start;
+      if (timePassed > duration) {
+        timePassed = duration;
+      }
+
+      drawFunction(menuObj, timePassed / duration);
+
+      if (timePassed < duration) {
+        requestAnimationFrame(animate);
+      }
+    });
+  }
+
+  /**
+   * Function increases opacity of menu object in accordance with progress.
+   *
+   * @param {XML/Node} menuObj - menu object which will be drown
+   * @param {Number} progress - real number from 0.0 till 1.0
+   */
+  function show(menuObj, progress) {
+    if (progress > 0) {
+      menuObj.css({
+        'opacity' : progress
+      });
+    } else {
+      menuObj.css({
+        'display' : 'block',
+        'opacity' : progress
+      });
+    }
+  }
+
+  /**
+   * Function decreases opacity of menu object in accordance with progress.
+   *
+   * @param {XML/Node} menuObj - menu object which will be drown
+   * @param {Number} progress - real number from 0.0 till 1.0
+   */
+  function hide(menuObj, progress) {
+    if (progress < 1) {
+      menuObj.css({
+        'opacity' : 1 - progress
+      });
+    } else {
+      menuObj.css({
+        'display' : 'none'
+      });
+    }
+  }
+
+  $(function() {
+    $('.menu-item__drop').hover(
+      function() {
+        var menu = $(this).children('.sub-menu');
+        drawMenu(menu, show, 200);
+      },
+      function() {
+        var menu = $(this).children('.sub-menu');
+        drawMenu(menu, hide, 200);
+      }
+    );
+  });
+})(jQuery);
