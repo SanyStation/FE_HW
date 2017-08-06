e4e73c55d438d94e26375983a9e52f0e3843c992
olko06141 <sanystation@gmail.com>
2017-08-07 02:11:35 +0300

    FE_JS_10_BONUS: Done!

--- fe_js_10_bonus/js/script.js
+++ fe_js_10_bonus/js/script.js
@@ -17,7 +17,7 @@ function drawMenu(menuObj, drawFunction, duration) {
       timePassed = duration;
     }
 
-    drawFunction(menuObj, timePassed / duration);
+    drawFunction(menuObj, timePassed / duration, 'rgb(225, 75, 75)');
 
     if (timePassed < duration) {
       requestAnimationFrame(animate);
@@ -25,16 +25,55 @@ function drawMenu(menuObj, drawFunction, duration) {
   });
 }
 
+function parseColor(color) {
+  "use strict";
+
+  //Short hex format (3 symbols, e.g. #fab)
+  var colorValue = color.match(/^#([0-9a-f]{3})$/i);
+  if (colorValue) {
+    return [
+      parseInt(colorValue.charAt(0), 16) * 0x11,
+      parseInt(colorValue.charAt(1), 16) * 0x11,
+      parseInt(colorValue.charAt(2), 16) * 0x11
+    ];
+  }
+
+  //Full hex format (6 symbols, e.g. #ffaabb)
+  colorValue = color.match(/^#([0-9a-f]{6})$/i);
+  if (colorValue) {
+    return [
+      parseInt(colorValue.substr(0, 2), 16),
+      parseInt(colorValue.substr(2, 2), 16),
+      parseInt(colorValue.substr(4, 2), 16)
+    ];
+  }
+
+  //RGB format
+  colorValue = color.match(/^rgb\s*\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\)$/i);
+  if (colorValue) {
+    return [colorValue[1], colorValue[2], colorValue[3]];
+  }
+}
+
 /**
  * Function increases opacity of menu object in accordance with progress.
  *
  * @param {XML|Node} menuObj - menu object which will be drown
  * @param {Number} progress - real number from 0.0 till 1.0
+ * @param colorStatic - current color of menu
  */
-function show(menuObj, progress) {
+function show(menuObj, progress, colorStatic) {
   "use strict";
   menuObj.style.display = 'block';
   menuObj.style.opacity = progress;
+
+  var color = parseColor(colorStatic);
+
+  var k = 0.3;
+  var red = Math.round(color[0] - color[0] * k * progress);
+  var green = Math.round(color[1] - color[1] * k * progress);
+  var blue = Math.round(color[2] - color[2] * k * progress);
+  menuObj.style.backgroundColor = 'rgb(' + red + ', ' + green + ', ' + blue + ')';
 }
 
 /**
@@ -42,11 +81,20 @@ function show(menuObj, progress) {
  *
  * @param {XML|Node} menuObj - menu object which will be drown
  * @param {Number} progress - real number from 0.0 to 1.0
+ * @param colorStatic - current color of menu
  */
-function hide(menuObj, progress) {
+function hide(menuObj, progress, colorStatic) {
   "use strict";
-  if (progress < 1) {
-    menuObj.style.opacity = 1 - progress;
+  progress = 1 - progress;
+  if (progress > 0) {
+    menuObj.style.opacity = progress;
+    var color = parseColor(colorStatic);
+
+    var k = 0.3;
+    var red = Math.round((color[0] - color[0] * k) + color[0] * k * (1 - progress));
+    var green = Math.round((color[1] - color[1] * k) + color[1] * k * (1 - progress));
+    var blue = Math.round((color[2] - color[2] * k) + color[2] * k * (1 - progress));
+    menuObj.style.backgroundColor = 'rgb(' + red + ', ' + green + ', ' + blue + ')';
   } else {
     menuObj.style.display = 'none';
   }
