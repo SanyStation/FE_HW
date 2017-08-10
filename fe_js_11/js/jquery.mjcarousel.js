aecb0d290225336c79d39dc8170768fb25da9f43
olko06141 <sanystation@gmail.com>
2017-08-10 20:22:03 +0300

    FE_JS_11: Done!

---
+++ fe_js_11/js/jquery.mjcarousel.js
@@ -0,0 +1,41 @@
+(function($) {
+  "use strict";
+
+  var defaults = {
+    buttonColor : '#000',
+    animationSpeed : 500
+  };
+
+  $.fn.mjcarousel = function(params) {
+    var options = $.extend(defaults, params);
+
+    var leftArrow = $('.carousel__arrow--left');
+    var rightArrow = $('.carousel__arrow--right');
+    var elementsList = $('.carousel__list');
+
+    leftArrow.css({'background-color' : options.buttonColor});
+    rightArrow.css({'background-color' : options.buttonColor});
+
+    var pixelsOffset = 200;
+    var currentLeftValue = 0;
+    var elementsCount = elementsList.find('li').length;
+    var minimumOffset = - (elementsCount - 3) * pixelsOffset;
+    var maximumOffset = 0;
+
+    leftArrow.click(function() {
+      if (currentLeftValue !== maximumOffset) {
+        currentLeftValue += 200;
+        elementsList.animate({ left : currentLeftValue + "px"}, options.animationSpeed);
+      }
+    });
+
+    rightArrow.click(function() {
+      if (currentLeftValue !== minimumOffset) {
+        currentLeftValue -= 200;
+        elementsList.animate({ left : currentLeftValue + "px"}, options.animationSpeed);
+      }
+    });
+
+    return this;
+  };
+})(jQuery);
---
+++ fe_js_11/js/script.js
@@ -0,0 +1,10 @@
+(function($) {
+  "use strict";
+
+  $(function() {
+    $('.carousel').mjcarousel({
+      buttonColor : '#524c94',
+      animationSpeed : 200
+    });
+  });
+})(jQuery);
