1d2c25b07097478427951be2916b5fd5dd1e226a
olko06141 <sanystation@gmail.com>
2017-10-08 21:15:47 +0300

    FE_JS_19: initial commit

---
+++ fe_js_19/gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+var cleanCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+var runSequence = require('run-sequence');
+var del = require('del');
+var minifyCss = require('gulp-minify-css');
+
+gulp.task('build', function() {
+  runSequence(['cleancss', 'compress'], function () {
+    console.log('BUILD DONE');
+  });
+});
+
+gulp.task('cleancss', function() {
+  return gulp.src('css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('compress', function(cb) {
+  pump([
+      gulp.src('js/*.js'),
+      uglify(),
+      gulp.dest('build/js')
+    ],
+    cb
+  );
+});
+
+gulp.task('clean', function() {
+  return del(['build']);
+});
+
+gulp.task('mincss', function(){
+  return gulp.src('main.css')
+    .pipe(minifyCss())
+    .pipe(gulp.dest('main'));
+});
